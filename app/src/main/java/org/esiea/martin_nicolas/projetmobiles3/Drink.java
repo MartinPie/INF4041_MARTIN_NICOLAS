64b1719c045b07405e51b62084a74908ba86443b
Nicobas <bnicobas@gmail.com>
2016-11-08 17:56:45 +0100

    Requette http json ok

---
+++ app/src/main/java/org/esiea/martin_nicolas/projetmobiles3/Drink.java
@@ -0,0 +1,128 @@
+package org.esiea.martin_nicolas.projetmobiles3;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by nicobas on 08/11/16.
+ */
+public class Drink {
+    private int id;
+    private String name;
+    private String category;
+    private String alcoholic;
+    private String glass;
+    private String instruction;
+    private String imgageUrl;
+    private HashMap<String, String> ingredients;
+
+    public Drink(JSONObject json, boolean isShort) {
+        try {
+            this.setId(json.getInt("idDrink"));
+            this.setName(json.getString("strDrink"));
+            this.setImgageUrl(json.getString("strDrinkThumb"));
+
+            if (!isShort) {
+                this.setCategory(json.getString("strCategory"));
+                this.setAlcoholic(json.getString("strAlcoholic"));
+                this.setGlass(json.getString("strGlass"));
+                this.setInstruction(json.getString("strInstructions"));
+
+                this.ingredients = new HashMap<String, String>();
+
+                for (int i = 1; i <= 15; i++) {
+                    String ingredient = json.getString("strIngredient" + i);
+                    String measure = json.getString("strMeasure" + i);
+
+                    if (ingredient != null) {
+                        this.ingredients.put(ingredient, measure);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAlcoholic() {
+        return alcoholic;
+    }
+
+    public void setAlcoholic(String alcoholic) {
+        this.alcoholic = alcoholic;
+    }
+
+    public String getGlass() {
+        return glass;
+    }
+
+    public void setGlass(String glass) {
+        this.glass = glass;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    public String getImgageUrl() {
+        return imgageUrl;
+    }
+
+    public void setImgageUrl(String imgageUrl) {
+        this.imgageUrl = imgageUrl;
+    }
+
+    public HashMap<String, String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(HashMap<String, String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Drink{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", alcoholic='" + alcoholic + '\'' +
+                ", glass='" + glass + '\'' +
+                ", instruction='" + instruction + '\'' +
+                ", imgageUrl='" + imgageUrl + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
+}
--- app/src/main/java/org/esiea/martin_nicolas/projetmobiles3/MainActivity.java
+++ app/src/main/java/org/esiea/martin_nicolas/projetmobiles3/MainActivity.java
@@ -1,13 +1,69 @@
 package org.esiea.martin_nicolas.projetmobiles3;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            URL url = new URL("http://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita");
+
+            HttpJsonRequest h = new HttpJsonRequest();
+            h.execute(url);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private class HttpJsonRequest extends AsyncTask<URL, Integer, Void> {
+        @Override
+        protected Void doInBackground(URL... arg0) {
+
+            ArrayList<Drink> drinks = new ArrayList<Drink>();
+
+            try {
+                URL url = arg0[0];
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+
+                String result = InputStreamOperations.InputStreamToString(inputStream);
+
+                // On récupère le JSON complet
+                JSONObject jsonObject = new JSONObject(result);
+                // On récupère le tableau d'objets qui nous concernent
+                JSONArray array = new JSONArray(jsonObject.getString("drinks"));
+
+                // Pour tous les objets on récupère les infos
+                for (int i = 0; i < array.length(); i++) {
+                    // On récupère un objet JSON du tableau
+                    JSONObject obj = new JSONObject(array.getString(i));
+
+                    drinks.add(new Drink(obj, true));
+                }
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
